2d851b9a1a672fb900f3399e3e50965c1a83abf8
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-02-05 12:56:54 -0300

    Create ServicoDTO.java

---
+++ ServicoDTO.java
@@ -0,0 +1,37 @@
+
+package DTO;
+
+
+public class ServiçoDTO {
+    private int id_servico;
+    private String nome_servico;
+    private double valor_servico;
+
+    public int getId_servico() {
+        return id_servico;
+    }
+
+    public void setId_servico(int id_servico) {
+        this.id_servico = id_servico;
+    }
+
+    public String getNome_servico() {
+        return nome_servico;
+    }
+
+    public void setNome_servico(String nome_servico) {
+        this.nome_servico = nome_servico;
+    }
+
+    public double getValor_servico() {
+        return valor_servico;
+    }
+
+    public void setValor_servico(double valor_servico) {
+        this.valor_servico = valor_servico;
+    }
+
+    
+    
+    
+}
